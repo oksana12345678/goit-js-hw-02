7f1ed960e494789a60a07325f9b52a66e67b57cb
oksana12345678 <oksana.tanashuk@gmail.com>
2024-01-10 12:49:29 +0100

    add

--- js/task-1.js
+++ js/task-1.js
@@ -1,35 +1,16 @@
-// function makeTransaction(quantity, pricePerDroid, customerCredits) {
-//   const totalPrice = quantity * pricePerDroid;
-//   const custCredits = customerCredits;
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+  const totalPrice = quantity * pricePerDroid;
+  const custCredits = customerCredits;
 
-//   if (totalPrice > custCredits) {
-//     return "Insufficient funds!";
-//   } else {
-//     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-//   }
-// }
-
-// console.log(makeTransaction(5, 3000, 23000));
-// console.log(makeTransaction(3, 1000, 15000));
-// console.log(makeTransaction(10, 5000, 8000));
-// console.log(makeTransaction(8, 2000, 10000));
-// console.log(makeTransaction(10, 500, 5000));
-
-function getEvenNumbers(start, end) {
-  let evenNumbers = [];
-
-  for (let i = start; i <= end; i++) {
-    if (i % 2 === 0) {
-      evenNumbers.push(i);
-    }
+  if (totalPrice > custCredits) {
+    return "Insufficient funds!";
+  } else {
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
   }
-
-  return evenNumbers;
 }
 
-
-console.log(getEvenNumbers(2, 5)); // [2, 4]
-console.log(getEvenNumbers(3, 11)); // [4, 6, 8, 10]
-console.log(getEvenNumbers(6, 12)); // [6, 8, 10, 12]
-console.log(getEvenNumbers(8, 8)); // [8]
-console.log(getEvenNumbers(7, 7)); // []
+console.log(makeTransaction(5, 3000, 23000));
+console.log(makeTransaction(3, 1000, 15000));
+console.log(makeTransaction(10, 5000, 8000));
+console.log(makeTransaction(8, 2000, 10000));
+console.log(makeTransaction(10, 500, 5000));
